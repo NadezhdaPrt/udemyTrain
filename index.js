f506be97a342c811a363b6249778391015834965
Nadezhda <president.ru@mail.ru>
2023-06-15 22:05:55 +0300

    added functions to fill the object

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,12 +15,44 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),   
+            b = prompt('На сколько оцените его?', '');
+            if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
+            personalMovieDB.movies[a] = b;               
+    }
+        else {
+            console.log('Error');
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {      
+            personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
+    }
+}
+writeYourGenres();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) 
+        console.log('Просмотрено довольно мало фильмов');
+    else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30)
+        console.log('Вы классический зритель');
+    else if (personalMovieDB.count >= 30)
+        console.log('Вы киноман!')
+    else console.log('Ошибка') 
+}
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) 
+        console.log(personalMovieDB);
+}
+showMyDB(personalMovieDB.privat);
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB)
+
